b512f3328b39aa5ae7b532c648e5e43d7680de0f
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-12-02 18:39:23 -0500

    Add background image

--- ventura-travel/src/App.tsx
+++ ventura-travel/src/App.tsx
@@ -1,13 +1,24 @@
 import NavBar from "./components/NavBar";
 import PostList from "./components/PostList";
+import island from "./assets/island.png";
+
+const backgroundStyle = {
+  backgroundImage: `url(${island})`,
+  backgroundSize: "cover", // Adjust as needed
+  backgroundPosition: "center center", // Adjust as needed
+  backgroundAttachment: "fixed",
+  backgroundRepeat: "no-repeat",
+  margin: 0, // Remove default body margin
+};
 
 function App() {
   return (
     <>
-      <div className="max-w-screen-xl mx-auto w-full">
+      <div style={backgroundStyle}>
         <NavBar />
-
-        <PostList />
+        <div className="mx-auto w-full pt-3 pb-3">
+          <PostList />
+        </div>
       </div>
     </>
   );
