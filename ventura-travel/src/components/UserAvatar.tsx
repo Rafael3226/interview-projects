26e50d9e53e4371d42eee0dfae233110a5e5da24
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-12-02 15:37:31 -0500

    Add User Avatar Component

---
+++ ventura-travel/src/components/UserAvatar.tsx
@@ -0,0 +1,22 @@
+import avatar from "../assets/avatar.png";
+
+export default function UserAvatar({
+  name,
+  img = avatar,
+  className,
+}: {
+  name: string;
+  img?: string;
+  className?: string;
+}) {
+  return (
+    <div
+      className={`flex items-center gap-4${className ? "" : " " + className}`}
+    >
+      <img className="w-10 h-10 rounded-full" src={img} alt="User Avatar" />
+      <div className="font-medium dark:text-white">
+        <div>{name}</div>
+      </div>
+    </div>
+  );
+}
