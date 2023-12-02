102dc93e729b27c1f7ef8028ff07228465e1187a
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-12-02 18:42:33 -0500

    Add comments to posts

--- ventura-travel/src/components/Post.tsx
+++ ventura-travel/src/components/Post.tsx
@@ -2,21 +2,32 @@ import { PostType } from "../core/types";
 import UserAvatar from "./UserAvatar";
 import useUser from "../hook/useUser";
 import { Card, CardBody, CardTitle } from "./card";
+import CommentList from "./CommentList";
+import useComments from "../hook/useComments";
 
 export type PostProps = {
+  id: PostType["id"];
   userId: PostType["userId"];
   title: PostType["title"];
   body: PostType["body"];
 };
 
-export default function Post({ title, body, userId }: PostProps) {
+export default function Post({ id, title, body, userId }: PostProps) {
   const user = useUser(userId);
+  const comments = useComments(id);
 
   return (
     <Card>
       <UserAvatar className="mb-2" name={user?.name || ""} />
       <CardTitle>{title}</CardTitle>
-      <CardBody>{body}</CardBody>
+      <CardBody>
+        {body}
+        <aside className="italic">
+          <span>{"Comment: " + comments.length}</span>
+          <span className="float-right">{"Contact: " + user?.email}</span>
+        </aside>
+      </CardBody>
+      <CommentList className="mt-2" comments={comments} />
     </Card>
   );
 }
--- ventura-travel/src/components/PostList.tsx
+++ ventura-travel/src/components/PostList.tsx
@@ -1,15 +1,37 @@
-import { useContext } from "react";
+import { MouseEvent, useContext, useState } from "react";
 import Post, { PostProps } from "./Post";
 import ApiDataContext from "../context";
 
 export default function PostList() {
+  const [limit, setLimit] = useState(3);
   const { posts } = useContext(ApiDataContext);
 
+  function handleClick(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+    setLimit((l) => l + 2);
+  }
+
+  const postRenderList = [...posts].slice(0, limit);
+  console.log(limit);
+
+  console.log(postRenderList);
+
   return (
-    <section className="flex flex-wrap justify-center px-4">
-      {posts.map((p) => (
-        <Post key={p.id} {...(p as PostProps)} />
-      ))}
+    <section>
+      <div className="flex flex-wrap justify-center px-4">
+        {postRenderList.map((p) => (
+          <Post key={p.id} {...(p as PostProps)} />
+        ))}
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <button
+          type="button"
+          onClick={handleClick}
+          className="bg-white text-blue-700 hover:text-white hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+        >
+          Load More Posts
+        </button>
+      </div>
     </section>
   );
 }
