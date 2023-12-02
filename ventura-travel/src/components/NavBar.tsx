88dc321cdb3ba983359f5edf82e03e8cc7192586
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-12-02 18:40:27 -0500

    Add company logo to navbar

--- ventura-travel/src/components/NavBar.tsx
+++ ventura-travel/src/components/NavBar.tsx
@@ -1,19 +1,14 @@
+import logo from "../assets/logo.svg";
+
 export default function NavBar() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="flex flex-wrap items-center justify-between p-4">
+    <nav className="bg-transparent border-b-2 border-gray-custom">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-60 p-4">
         <a
-          href="https://flowbite.com/"
+          href="https://www.venturatravel.org/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Ventura
-          </span>
+          <img src={logo} className="h-8" alt="Ventura travel logo." />
         </a>
       </div>
     </nav>
