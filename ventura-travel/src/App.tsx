a27017ab2033267cd0fe7896f9b361712f7ecbcf
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-12-02 20:03:06 -0500

    Update Background style

--- ventura-travel/src/App.tsx
+++ ventura-travel/src/App.tsx
@@ -1,24 +1,12 @@
 import NavBar from "./components/NavBar";
 import PostList from "./components/PostList";
-import island from "./assets/island.png";
-
-const backgroundStyle = {
-  backgroundImage: `url(${island})`,
-  backgroundSize: "cover", // Adjust as needed
-  backgroundPosition: "center center", // Adjust as needed
-  backgroundAttachment: "fixed",
-  backgroundRepeat: "no-repeat",
-  margin: 0, // Remove default body margin
-};
 
 function App() {
   return (
     <>
-      <div style={backgroundStyle}>
-        <NavBar />
-        <div className="mx-auto w-full pt-3 pb-3">
-          <PostList />
-        </div>
+      <NavBar />
+      <div className="mx-auto w-full pt-3 pb-3">
+        <PostList />
       </div>
     </>
   );
